737a5bc1ede63d3c51991f687e6c12eb654444b7
bang0123 <nikolajbang1995@gmail.com>
2016-11-02 13:15:02 +0100

    start på restful WCF

---
+++ RestWCF/IService1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestWCF
+{
+   [ServiceContract]
+    public interface IService1
+    {
+
+        //[OperationContract]
+        //[WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "customers/")]
+        
+
+        
+    }
+
+
+   
+}
